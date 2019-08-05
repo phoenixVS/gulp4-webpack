2588879e694cb3fc2d2200bbd7dfa35c00871e08
Valentin Shapoval <valentin.vad.sh@gmail.com>
2019-08-06 00:16:28 +0300

    All worked

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# gulp4-webpack
+# gulp4-webpack4
+
+## To make it work make sure that you have installed
+* yarn
+* gulp-cli
+
+## To install  this progect
+1. clone this repo
+2. yarn
+3. gulp
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const config = {
+    entry: {
+        about: "./src/assets/scripts/about.js",
+        auth: "./src/assets/scripts/auth.js",
+        works: "./src/assets/scripts/works.js",
+        blog: "./src/assets/scripts/blog.js"
+    },
+    output: {
+        filename: "[name].bundle.js"
+    },
+    optimization: {
+        minimizer: [new TerserPlugin()],
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    // plugins: [
+    //     new UglifyJSPlugin({
+    //         sourceMap: true
+    //     })
+    // ]
+};
+
+module.exports = config;
